e8ac06b76fbad94815414bb7209be115baa1b8e3
Denis Nazarov <d@denisnazarov.com>
2019-11-12 13:33:33 -0500

    add db migration to create initial tables

---
+++ README.md
@@ -0,0 +1,7 @@
+# ar-posts-by-tag
+
+Ingests Arweave transactions tagged with a certain tag.
+
+## Migrations
+
+Run `npm run migrate up` to run PostgreSQL migrations.
---
+++ migrations/1573579268987_first-migration.js
@@ -0,0 +1,82 @@
+/* eslint-disable no-undef */
+/* eslint-disable camelcase */
+
+// PRIMARY KEYS
+// transaction_id
+// block_id
+// block_height
+// owner_id
+// app_name
+
+// TABLES
+// user -- don't need to start
+// block
+// - indep_hash (string)
+// - height (integer)
+// - timestamp (timestamp)
+// - raw_data (JSON)
+// transaction
+// - id (string)
+// - block_hash (string)
+// - raw_data (JSON)
+// - owner (string)
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.createTable("blocks", {
+      hash: {
+          type: 'varchar(64)',
+          notNull: true,
+          primaryKey: true,
+          unique: true,
+      },
+      height: {
+          type: 'integer',
+          notNull: true,
+          unique: true,
+      },
+      timestamp: {
+          type: 'timestamp',
+          notNull: true,
+      },
+      rawData: {
+          type: 'jsonb',
+          notNull: true,
+
+      }
+  });
+  pgm.createIndex('blocks', 'height');
+
+  pgm.createTable("transactions", {
+      id: {
+          type: 'varchar(43)',
+          notNull: true,
+          primaryKey: true,
+          unique: true,
+      },
+      blockHash: {
+          type: 'varchar(64)',
+          notNull: true,
+          references: 'blocks',
+          onDelete: 'cascade',
+          unique: true,
+
+      },
+      rawData: {
+          type: 'jsonb',
+          notNull: true,
+      },
+      owner: {
+          type: 'varchar(689)',
+          notNull: true,
+      }
+  });
+  pgm.createIndex('transactions', 'blockHash');
+  pgm.createIndex('transactions', 'owner');
+};
+
+exports.down = pgm => {
+    pgm.dropTable("transactions")
+    pgm.dropTable("blocks")
+};
